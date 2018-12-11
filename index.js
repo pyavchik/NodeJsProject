29f52e5b4b40f53c7f1538bc8fd51af1d0e8511c
pyavchik <pyavchik@gmail.com>
2018-12-11 21:52:05 +0200

    addet movie rating, added headers to request

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
-const URL = 'https://www.imdb.com/title/tt0384537/';
+const URL = 'https://www.imdb.com/title/tt0102926/?ref_=nv_sr_1';
 
 (async () =>{
 
-    const response = await request(URL);
+    const response = await request({
+        uri: URL,
+        headers:{
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'www.imdb.com',
+            'Referer': 'https://www.imdb.com/title/tt0384537/',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
+        },
+        gzip: true
+    },
+        );
     let $ = cheerio.load(response);
     let title = $('div[class="title_wrapper"] > h1').text();
-    console.log(title);
+    let rating = $('span[itemprop="ratingValue"]').text();
+    console.log(title, rating);
 
 })();
