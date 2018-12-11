1e1346117ab029fa2ae72b075fee65c802afa6b6
pyavchik <pyavchik@gmail.com>
2018-12-11 21:17:47 +0200

    added simple scpraping project

---
+++ index.js
@@ -0,0 +1,13 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+const URL = 'https://www.imdb.com/title/tt0384537/';
+
+(async () =>{
+
+    const response = await request(URL);
+    let $ = cheerio.load(response);
+    let title = $('div[class="title_wrapper"] > h1').text();
+    console.log(title);
+
+})();
